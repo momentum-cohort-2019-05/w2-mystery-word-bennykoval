044e1524b340d738070c5cd7995d58be59769339
bennykoval <bethanykoval@gmail.com>
2019-06-07 14:42:55 -0400

    Print intro and difficulty choice

---
+++ mystery_word.py
@@ -0,0 +1,5 @@
+print("Welcome to Mystery Word!")
+level_difficulty_str = input("Choose a difficulty level: (E)asy, (M)edium, or (H)AHAHAHAHA?")
+print("Good luck!")
+
+#if level_difficulty_str == "E":
