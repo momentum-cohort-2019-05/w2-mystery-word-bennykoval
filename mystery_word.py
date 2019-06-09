64c02c45bb95e526b77a46457261a0652d3cd7ab
bennykoval <bethanykoval@gmail.com>
2019-06-09 18:57:35 -0400

    Transfer everything to one big file because i do not have the patience right now

--- mystery_word.py
+++ mystery_word.py
@@ -1,31 +1,31 @@
-import user_selections 
+# import user_selections 
 
-def play_game(word):
-    chances: 8
-    guesses = ""
+# def play_game(word):
+#     chances: 8
+#     guesses = ""
     
-    while chances > 0:
-        failed = 0
+#     while chances > 0:
+#         failed = 0
 
-        for letter in word:
-            if letter in guesses:
-                print(letter)
-            else:
-                print("_")
-                failed += 1
+#         for letter in word:
+#             if letter in guesses:
+#                 print(letter)
+#             else:
+#                 print("_")
+#                 failed += 1
 
-            if failed == 0:
-                print("You did it!")
+#             if failed == 0:
+#                 print("You did it!")
 
-            guesses = input("Guess a letter: ")
-            total_guesses += guesses
+#             guesses = input("Guess a letter: ")
+#             total_guesses += guesses
 
-            if guesses not in word:
-                chances -= 1
-                print(chances, " more guesses remaining!")
+#             if guesses not in word:
+#                 chances -= 1
+#                 print(chances, " more guesses remaining!")
 
-                if chances == 0:
-                    print(word, " was your word. Sorry!")
-                    get_permission()
+#                 if chances == 0:
+#                     print(word, " was your word. Sorry!")
+#                     get_permission()
 
                 
--- user_selections.py
+++ user_selections.py
@@ -69,3 +69,31 @@ def get_difficulty(file):
     return word_list
 
 get_permission()
+
+def play_game(word):
+    chances: 8
+    guesses = ""
+    
+    while chances > 0:
+        failed = 0
+
+        for letter in word:
+            if letter in guesses:
+                print(letter)
+            else:
+                print("_")
+                failed += 1
+
+            if failed == 0:
+                print("You did it!")
+
+            guesses = input("Guess a letter: ")
+            total_guesses += guesses
+
+            if guesses not in word:
+                chances -= 1
+                print(chances, " more guesses remaining!")
+
+                if chances == 0:
+                    print(word, " was your word. Sorry!")
+                    get_permission()
