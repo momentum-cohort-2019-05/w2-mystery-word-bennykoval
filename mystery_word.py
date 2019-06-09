6a672a7e421c9d5dc7a1ab85c3004b8dbbced86a
bennykoval <bethanykoval@gmail.com>
2019-06-09 15:48:08 -0400

    I am going to cry

--- mystery_word.py
+++ mystery_word.py
@@ -4,19 +4,8 @@ import random
 print("Welcome to Mystery Word! When prompted for input, please type the first letter of your choice.")
 player_consent_str = input("Would you like to guess the word I'm thinking of? Please type (Y)es or (N)o: ").upper()
 
-def get_permission():
-    if player_consent_str == "Y":
-        start_game = True
-        start_game = get_edition(file)
-        return start_game
-
-    if player_consent_str == "N":
-        start_game = False
-        return print("Type quit() or Command-Z plus Return to exit. See you later!")
-
-    if player_consent_str is not "N" or "Y":
-        print("Sorry, I don't understand. Please try again: ")
-        return player_consent_str
+chosen_file = "common_words.txt" or "pokemon_edition.txt" or "archaic_words.txt"
+file = chosen_file
 
 def get_edition(file):
     """Take user edition choice, store in chosen_file, move on to difficulty selection"""
@@ -25,33 +14,42 @@ def get_edition(file):
     if edition_choice_str == "C":
         chosen_file = "common_words.txt"
         game_cont = get_difficulty(chosen_file)
-    return game_cont
     
-    if edition_choice_str == "A":
+    elif edition_choice_str == "A":
         chosen_file = "archaic_words.txt"
         game_cont = get_difficulty(chosen_file)
-    return game_cont
     
-    if edition_choice_str == "P":
+    elif edition_choice_str == "P":
         chosen_file = "pokemon_edition.txt"
         game_cont = get_difficulty(chosen_file)
-    return game_cont
 
-    if edition_choice_str is not "C" or "A" or "P":
+    else:
         print("We do not have that edition in stock yet. Try another for now: ")
     return get_edition(chosen_file)
+
+def get_permission():
+    if player_consent_str == "Y":
+        start_game = get_edition(file)
+        return start_game
+
+    if player_consent_str == "N":
+        return print("Type quit() or Command-Z plus Return to exit. See you later!")
+
+    if player_consent_str is not "N" or "Y":
+        print("Sorry, I don't understand. Please try again: ")
+        return player_consent_str
 
 def get_difficulty(file):
 
     level_difficulty_str = input("Choose a difficulty level: (E)asy, (M)edium, or (H)ard? ").upper() 
 
-    if level_difficulty_str == "E" or "EASY":
+    if level_difficulty_str == "E":
         char_range = range(4,7)
 
-    elif level_difficulty_str == "M" or "MEDIUM":
+    elif level_difficulty_str == "M":
         char_range = range(6,9)
 
-    elif level_difficulty_str == "H" or "HARD":
+    elif level_difficulty_str == "H":
         char_range = range(8, 46)
         #the longest english word is 45 characters
         #it's pneumonoultramicroscopicsilicovolcanoconiosis if anyone cares
