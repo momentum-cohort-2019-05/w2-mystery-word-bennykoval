b662eca9460b66ecc8fde046c95114c71347c71a
bennykoval <bethanykoval@gmail.com>
2019-06-09 18:45:27 -0400

    Add play game function, set chances amount

--- mystery_word.py
+++ mystery_word.py
@@ -1,2 +1,24 @@
 import user_selections 
 
+def restart(get_permission):
+
+def play_game(word):
+    guesses = ""
+    chances: 8
+
+    while chances > 0:
+        failed = 0
+
+        for letter in word:
+            if letter in guesses:
+                print(letter)
+            else:
+                print("_")
+                failed += 1
+
+            if failed == 0:
+                print("You did it!")
+
+            guesses = input("Guess a letter: ")
+            total_guesses += guesses
+                
