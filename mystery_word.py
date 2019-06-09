9dfabe63cc2b6460b5fab279cdf9e55e88e14c1e
bennykoval <bethanykoval@gmail.com>
2019-06-09 16:11:14 -0400

    Did a dingy

--- mystery_word.py
+++ mystery_word.py
@@ -4,32 +4,9 @@ import random
 print("Welcome to Mystery Word! When prompted for input, please type the first letter of your choice.")
 player_consent_str = input("Would you like to guess the word I'm thinking of? Please type (Y)es or (N)o: ").upper()
 
-chosen_file = "common_words.txt" or "pokemon_edition.txt" or "archaic_words.txt"
-file = chosen_file
-
-def get_edition(file):
-    """Take user edition choice, store in chosen_file, move on to difficulty selection"""
-    edition_choice_str = input("Choose an edition: (C)ommon, (A)rchaic, or (P)okemon: ").upper()
-
-    if edition_choice_str == "C":
-        chosen_file = "common_words.txt"
-        game_cont = get_difficulty(chosen_file)
-    
-    elif edition_choice_str == "A":
-        chosen_file = "archaic_words.txt"
-        game_cont = get_difficulty(chosen_file)
-    
-    elif edition_choice_str == "P":
-        chosen_file = "pokemon_edition.txt"
-        game_cont = get_difficulty(chosen_file)
-
-    else:
-        print("We do not have that edition in stock yet. Try another for now: ")
-    return get_edition(chosen_file)
-
 def get_permission():
     if player_consent_str == "Y":
-        start_game = get_edition(file)
+        start_game = get_edition()
         return start_game
 
     if player_consent_str == "N":
@@ -39,7 +16,29 @@ def get_permission():
         print("Sorry, I don't understand. Please try again: ")
         return player_consent_str
 
-def get_difficulty(file):
+def get_edition():
+    """Take user edition choice, store in chosen_file, move on to difficulty selection"""
+    edition_choice_str = input("Choose an edition: (C)ommon, (A)rchaic, or (P)okemon: ").upper()
+
+    edition_choice_list = [ "C", "A", "P" ] 
+
+    if edition_choice_str == "C":
+        chosen_file = "common_words.txt"
+        return get_difficulty(chosen_file)
+    
+    if edition_choice_str == "A":
+        chosen_file = "archaic_words.txt"
+        return get_difficulty(chosen_file)
+    
+    if edition_choice_str == "P":
+        chosen_file = "pokemon_edition.txt"
+        return get_difficulty(chosen_file)
+
+    if edition_choice_str is not edition_choice_list:
+        print("We do not have that edition in stock yet. Try another for now: ")
+        return get_edition()
+
+def get_difficulty(chosen_file):
 
     level_difficulty_str = input("Choose a difficulty level: (E)asy, (M)edium, or (H)ard? ").upper() 
 
@@ -64,3 +63,5 @@ def get_difficulty(file):
             if len(word.strip()) in char_range
         ]
     return word_list
+
+get_permission()
